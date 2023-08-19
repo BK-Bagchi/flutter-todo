25479cb57c2b2e8e30d3dcb3a72ac3c8bbb5d6ab
BK-Bagchi <bkbagchi.dipto@gmail.com>
2023-08-19 10:02:18 +0600

    flutter todo application

---
+++ lib/main.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TodoApp());
+}
+
+class Todo {
+  final String title;
+  bool isDone;
+
+  Todo({
+    required this.title,
+    this.isDone = false,
+  });
+}
+
+class TodoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo App',
+      home: TodoListScreen(),
+    );
+  }
+}
+
+class TodoListScreen extends StatefulWidget {
+  @override
+  _TodoListScreenState createState() => _TodoListScreenState();
+}
+
+class _TodoListScreenState extends State<TodoListScreen> {
+  List<Todo> todos = [];
+
+  void addTodo(String title) {
+    setState(() {
+      todos.add(Todo(title: title));
+    });
+  }
+
+  void toggleTodoStatus(int index) {
+    setState(() {
+      todos[index].isDone = !todos[index].isDone;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todo App'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: todos.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(
+                    todos[index].title,
+                    style: TextStyle(
+                      decoration: todos[index].isDone
+                          ? TextDecoration.lineThrough
+                          : TextDecoration.none,
+                    ),
+                  ),
+                  trailing: Checkbox(
+                    value: todos[index].isDone,
+                    onChanged: (_) => toggleTodoStatus(index),
+                  ),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Enter a new task',
+                    ),
+                    onSubmitted: (taskName) {
+                      if (taskName.trim().isNotEmpty) {
+                        addTodo(taskName);
+                      }
+                    },
+                  ),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        String newTask = '';
+                        return AlertDialog(
+                          title: Text('Add Task'),
+                          content: TextField(
+                            onChanged: (taskName) {
+                              newTask = taskName;
+                            },
+                            decoration: InputDecoration(
+                              hintText: 'Enter task name',
+                            ),
+                          ),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(context); // Close the dialog
+                              },
+                              child: Text('Cancel'),
+                            ),
+                            ElevatedButton(
+                              onPressed: () {
+                                if (newTask.trim().isNotEmpty) {
+                                  addTodo(newTask);
+                                  Navigator.pop(context); // Close the dialog
+                                }
+                              },
+                              child: Text('Add'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  child: Text('Add Task'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
